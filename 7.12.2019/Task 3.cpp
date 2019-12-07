23ee76b8e54d81d9348896830030f49a10b54449
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2019-12-07 21:38:42 +0300

    Add files via upload

---
+++ 7.12.2019/Task 3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int fib(int f)
+{
+    if (f < 2)
+        return f;
+    else if (f == 2)
+        return 1;
+    else if (f % 2 == 1)
+        return pow(fib((f - 1) / 2), 2) + pow(fib((f - 1) / 2 + 1), 2);
+    else
+        return pow(fib(f / 2 + 1), 2) - pow(fib(f / 2 - 1), 2);
+}
+
+int main()
+{
+    int f;
+    cin >> f;
+
+    if (f < 0)
+        cout << "/GG";
+    else
+        cout << fib(f);
+
+    return 0;
+}
