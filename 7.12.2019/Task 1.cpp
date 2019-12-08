eae11b3c3b6c57ae6be8e6aa520f5f1b0f8f86d1
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2019-12-08 20:57:13 +0300

    Update Task 1.cpp

--- 7.12.2019/Task 1.cpp
+++ 7.12.2019/Task 1.cpp
@@ -1,3 +1,8 @@
+/*
+Напишите функцию находящую корень монотонной вещественной функции f бинарным поиском(бисекцией). 
+Можно считать что корень обязательно существует. Корнем будем считать значение x, такое что |f(x)|<10^(-6).  
+В комментариях напишите, почему решение- эффективное
+*/
 //X^3-3X^2+3X-1
 
 #include <iostream>
@@ -6,7 +11,7 @@
 
 using namespace std;
 
-int function(int a, int b, int c, int d)
+double function(double a, double b, double c, double d)
 {
     double n0 = -1000;
     double nn = 1000;
@@ -24,22 +29,27 @@ int function(int a, int b, int c, int d)
             n0 = x;
     }
 
-    return -1000;
+    return -1001;
 }
 
 int main()
 {
     cout << "Enter a, b, c, d, if f(x) = a*X^3 + b*X^2 + c*X + d" << endl << endl;
 
-    int a, b, c, d;
+    double a, b, c, d;
     cin >> a >> b >> c >> d;
 
     double p = function(a, b, c, d);
 
-    if (p == -1000)
-        cout << "x not included in [-100; 100]";
+    if (p == -1001)
+        cout << "x not included in [-1000; 1000]";
     else
         cout << p;
 
     return 0;
 }
+
+/*
+Мы использульзуем метод бисекции, а он довольно эффектиен, да и по-другому задачу сложно решить, чтобы был
+этот метод
+*/ 
