d530fd25e50098a2d0ab1516128e06c6cf424434
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2019-12-10 22:38:38 +0300

    Update Task 4-6.cpp

--- 7.12.2019/Task 4-6.cpp
+++ 7.12.2019/Task 4-6.cpp
@@ -22,12 +22,12 @@ using namespace std;
 struct LongInt
 {
     int n;
-    char* arr;
+    int* arr;
    
-    LongInt(char* a, int b)
+    LongInt(int* a, int b)
     {
         n = b;
-        char* arr = new char[n];
+        int* arr = new int[n];
         for (int i; i < n; i++)
             arr[i] = a[i];
     }
@@ -35,7 +35,7 @@ struct LongInt
     LongInt(LongInt &X)
     {
         n = X.n;
-        char* arr = new char[n];
+        int* arr = new int[n];
         for (int i; i < n; i++)
             arr[i] = X.arr[i];
     }
@@ -50,84 +50,84 @@ struct LongInt
 LongInt operator+ (LongInt a, LongInt b)
 {
    
-    if ((int)a.n > (int)b.n)
+    if (a.n > b.n)
     {
-        char* temp = new char[(int)a.n + 1];
-        for (int i = 0; i <= (int)a.n; i++)
+        int* temp = new int[a.n + 1];
+        for (int i = 0; i <= a.n; i++)
             temp[i] = 0;
            
-        int j = (int)a.n - 1;
-        int jj = (int)b.n - 1;
+        int j = a.n - 1;
+        int jj = b.n - 1;
         while (j >= 0)
         {
             if (jj >= 0)
             {
-                int p = (int)a.arr[j] + (int)b.arr[jj] + (int)temp[j];
+                int p = a.arr[j] + b.arr[jj] + temp[j];
                 if (p > 10)
                 {
-                    if (j != (int)a.n - 1)
+                    if (j != a.n - 1)
                         temp[j + 1] = p % 10;
                     else
-                        temp[j + 1] = p % 10 + (int)temp[j];
+                        temp[j + 1] = p % 10 + temp[j];
                     temp[j] = p / 10;
                 }
             }
             else
-                temp[j + 1] = (int)a.n + (int)temp[j];
+                temp[j + 1] = a.n + temp[j];
            
             j--;
             jj--;
         }
    
         if (temp[0] != 0)
-            return LongInt(temp, (int)a.n + 1);
+            return LongInt(temp, a.n + 1);
         else
         {
-            char* temp2 = new char[a.n];
-            for (int i = 0; i < (int)a.n; i++)
+            int* temp2 = new int[a.n];
+            for (int i = 0; i < a.n; i++)
                 temp2[i] = temp[i + 1];
                
-            return LongInt(temp2, (int)a.n);
+            return LongInt(temp2, a.n);
         }
     }
     else
     {
-        char* temp = new char[(int)b.n + 1];
-        for (int i = 0; i <= (int)b.n; i++)
+        int* temp = new int[b.n + 1];
+        for (int i = 0; i <= b.n; i++)
             temp[i] = 0;
            
-        int j = (int)b.n - 1;
-        int jj = (int)a.n - 1;
+        int j = b.n - 1;
+        int jj = a.n - 1;
         while (j >= 0)
         {
             if (jj >= 0)
             {
-                int p = (int)b.arr[j] + (int)a.arr[jj] + (int)temp[j];
+                int p = b.arr[j] + a.arr[jj] + temp[j];
                 if (p > 10)
                 {
-                    if (j != (int)b.n - 1)
+                    if (j != b.n - 1)
                         temp[j + 1] = p % 10;
                     else
-                        temp[j + 1] = p % 10 + (int)temp[j];
+                        temp[j + 1] = p % 10 + temp[j];
                     temp[j] = p / 10;
                 }
             }
             else
-                temp[j + 1] = (int)b.n + temp[j];
+                temp[j + 1] = b.n + temp[j];
            
             j--;
             jj--;
         }
        
         if (temp[0] != 0)
-            return LongInt(temp, (int)b.n + 1);
+            return LongInt(temp, b.n + 1);
         else
         {
-            char* temp2 = new char[b.n];
+            int* temp2 = new int[b.n];
             for (int i = 0; i < b.n; i++)
                 temp2[i] = temp[i + 1];
                
-            return LongInt(temp2, (int)b.n);
+            return LongInt(temp2, b.n);
         }
     }
 };
@@ -159,19 +159,19 @@ LongInt operator^ (LongInt a, LongInt b)
 
 bool operator> (Rational a, Rational b)
 {
-    if ((int)a.n != (int)b.n)
+    if (a.n != b.n)
     {
-        if ((int)a.n > (int)b.n)
+        if (a.n > b.n)
             return 1;
         else 
             return 0;
     }
     else
     {
-        int i = (int)a.n - 1;
+        int i = a.n - 1;
         while (i >= 0)
         {
-            if ((int)a.arr[i] > (int)b.arr[i])
+            if (a.arr[i] > b.arr[i])
         }
     }
 };
@@ -187,8 +187,8 @@ istream& operator>> (std::istream& in, LongInt &a)
     string str;
     in >> str;
     a.n = str.size();
-    a.arr = new char[(int)a.n];
-    for (int i = 0; i < (int)a.n; i++)
+    a.arr = new int[a.n];
+    for (int i = 0; i < a.n; i++)
         a.arr[i] = str[i];
    
     return in;
@@ -199,10 +199,9 @@ int main()
     LongInt a();
     LongInt b();
     cin >> a >> b;
-    cout << a + b;
    
-    /*cout << "+   " << LongInt(arr, n) + LongInt(arr2, n2) << endl;
-    cout << "-   " << LongInt(arr, n) - LongInt(arr2, n2) << endl;
+    cout << "+   " << a + b << endl;
+    /*cout << "-   " << LongInt(arr, n) - LongInt(arr2, n2) << endl;
     cout << "*   " << LongInt(arr, n) * LongInt(arr2, n2) << endl;
     cout << "/   " << LongInt(arr, n) / LongInt(arr2, n2) << endl;
     cout << "%   " << LongInt(arr, n) % LongInt(arr2, n2) << endl;
