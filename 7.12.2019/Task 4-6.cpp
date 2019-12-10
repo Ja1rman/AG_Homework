0f80895b442a413c11c1f37ebcdb6c5b58c3bac7
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2019-12-10 14:46:21 +0300

    Update Task 4-6.cpp

--- 7.12.2019/Task 4-6.cpp
+++ 7.12.2019/Task 4-6.cpp
@@ -1,6 +1,6 @@
 /*
-Реализуйте структуру LongInt, позволяющую использовать длинные натуральные числа, 
-а также пример использования. 
+Реализуйте структуру LongInt, позволяющую использовать длинные натуральные числа,
+а также пример использования.
 Реализовать:
 конструкторы
 конструктор копий
@@ -24,15 +24,15 @@ struct LongInt
 {
     int n;
     char* arr;
-    
-    LongInt(char* a, int b)
+   
+    LongInt(char[] a, int b)
     {
         n = b;
         char* arr = new char[n];
         for (int i; i < n; i++)
             arr[i] = a[i];
     }
-    
+   
     LongInt(LongInt &X)
     {
         n = X.n;
@@ -40,7 +40,7 @@ struct LongInt
         for (int i; i < n; i++)
             arr[i] = X.arr[i];
     }
-    
+   
     ~LongInt()
     {
         delete [] arr;
@@ -50,13 +50,13 @@ struct LongInt
 
 LongInt operator+ (LongInt a, LongInt b)
 {
-    
+   
     if ((int)a.n > (int)b.n)
     {
         char* temp = new char[(int)a.n + 1];
         for (int i = 0; i <= (int)a.n; i++)
             temp[i] = 0;
-            
+           
         int j = (int)a.n - 1;
         int jj = (int)b.n - 1;
         while (j >= 0)
@@ -75,11 +75,11 @@ LongInt operator+ (LongInt a, LongInt b)
             }
             else
                 temp[j + 1] = (int)a.n + temp[j];
-            
+           
             j--;
             jj--;
         }
-    
+   
         if (temp[0] != 0)
             return LongInt(temp, a.n + 1);
         else
@@ -87,7 +87,7 @@ LongInt operator+ (LongInt a, LongInt b)
             char* temp2 = new char[a.n];
             for (int i = 0; i < a.n; i++)
                 temp2[i] = temp[i + 1];
-                
+               
             return LongInt(temp2, a.n);
         }
     }
@@ -96,7 +96,7 @@ LongInt operator+ (LongInt a, LongInt b)
         char* temp = new char[b.n + 1];
         for (int i = 0; i <= b.n; i++)
             temp[i] = 0;
-            
+           
         int j = (int)b.n - 1;
         int jj = (int)a.n - 1;
         while (j >= 0)
@@ -115,11 +115,11 @@ LongInt operator+ (LongInt a, LongInt b)
             }
             else
                 temp[j + 1] = (int)b.n + temp[j];
-            
+           
             j--;
             jj--;
         }
-        
+       
         if (temp[0] != 0)
             return LongInt(temp, b.n + 1);
         else
@@ -127,39 +127,39 @@ LongInt operator+ (LongInt a, LongInt b)
             char* temp2 = new char[b.n];
             for (int i = 0; i < b.n; i++)
                 temp2[i] = temp[i + 1];
-                
-            return LongInt(temp, b.n);
+               
+            return LongInt(temp2, b.n);
         }
     }
 };
 
 LongInt operator- (LongInt a, LongInt b)
 {
-    
+   
 };
 
 LongInt operator* (LongInt a, LongInt b)
 {
-    
+   
 };
 
 LongInt operator/ (LongInt a, LongInt b)
 {
-    
+   
 };
 
 LongInt operator% (LongInt a, LongInt b)
 {
-    
+   
 };
 
 LongInt operator^ (LongInt a, LongInt b)
 {
-    
+   
 };
 
 ostream& operator<< (std::ostream& out, const LongInt &a)
-{ 
+{
     string str = string(a.arr);
     return out << str;
 };
@@ -168,30 +168,27 @@ istream& operator>> (std::istream& in, LongInt &a)
 {
     string str;
     in >> str;
-    int n = str.size();
-    char* arr = new char[n];
-    for (int i = 0; i < n; i++)
-        arr[i] = str[i];
-    
-    return LongInt(arr, n);
+    a.n = str.size();
+    a.arr = new char[a.n];
+    for (int i = 0; i < a.n; i++)
+        a.arr[i] = str[i];
+   
+    return in;
 };
 
 int main()
 {
-    LongInt a;
-    LongInt b;
+    LongInt a();
+    LongInt b();
     cin >> a >> b;
     cout << a + b;
-    
+   
     /*cout << "+   " << LongInt(arr, n) + LongInt(arr2, n2) << endl;
     cout << "-   " << LongInt(arr, n) - LongInt(arr2, n2) << endl;
     cout << "*   " << LongInt(arr, n) * LongInt(arr2, n2) << endl;
     cout << "/   " << LongInt(arr, n) / LongInt(arr2, n2) << endl;
     cout << "%   " << LongInt(arr, n) % LongInt(arr2, n2) << endl;
     cout << "pow   " << LongInt(arr) ^ k << endl;*/
-    
-    delete [] arr;
-    delete [] arr2;
-    
+   
     return 0;
 }
