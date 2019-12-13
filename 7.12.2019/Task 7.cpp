f210d4bf1acebdffd074280428087f8567ef8368
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2019-12-13 21:34:18 +0300

    Update Task 7.cpp

--- 7.12.2019/Task 7.cpp
+++ 7.12.2019/Task 7.cpp
@@ -12,9 +12,9 @@ int res(int* x, int* zoloto, int* nrj, int n0, int nn)
         i = (n0 + nn) / 2;
         if ((x[nn] - x[mm] >= nrj[nn + 1] - nrj[mm]) && (x[nn - 1] - x[mm] <= nrj[nn] - nrj[mm]) && (nn <= n))
             return zoloto[nn] - zoloto[mm];
-        else if ((x[nn] - x[mm] >= nrj[nn + 1] - nrj[mm]) && (x[nn - 1] - x[mm] >= nrj[nn] - nrj[mm]) && (nn <= n))
-            n0 = i + 1;
         else if ((x[nn] - x[mm] <= nrj[nn + 1] - nrj[mm]) && (x[nn - 1] - x[mm] <= nrj[nn] - nrj[mm]) && (nn <= n))
+            n0 = i + 1;
+        else if ((x[nn] - x[mm] >= nrj[nn + 1] - nrj[mm]) && (x[nn - 1] - x[mm] >= nrj[nn] - nrj[mm]) && (nn <= n))
             nn = i; 
         else if (nn >= n);
             return zoloto[nn] - zoloto[mm];
