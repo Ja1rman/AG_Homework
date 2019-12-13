058a302e716d6abeedcee2ac4476ae1c0ac1ffce
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2019-12-13 21:15:38 +0300

    Update Task 7.cpp

--- 7.12.2019/Task 7.cpp
+++ 7.12.2019/Task 7.cpp
@@ -2,21 +2,21 @@
 
 using namespace std;
 
-int res(int* x, int* zoloto, int* nrj, int n0, int nn, int mmm, int n)
+int res(int* x, int* zoloto, int* nrj, int n0, int nn, int mm, int n)
 {
     int i;
 
     while (n0 < nn)
     {
         i = (n0 + nn) / 2;
-        if ((x[nn] - x[mmm] >= nrj[nn + 1] - nrj[mmm]) && (x[nn - 1] - x[mmm] <= nrj[nn] - nrj[mmm]) && (nn <= n))
-            return zoloto[nn] - zoloto[mmm];
-        else if ((x[nn] - x[mmm] >= nrj[nn + 1] - nrj[mmm]) && (x[nn - 1] - x[mmm] >= nrj[nn] - nrj[mmm]) && (nn <= n))
+        if ((x[nn] - x[mm] > nrj[nn + 1] - nrj[mm]) && (x[nn - 1] - x[mm] <= nrj[nn] - nrj[mm]) && (nn <= n))
+            return zoloto[nn] - zoloto[mm];
+        else if ((x[nn] - x[mm] >= nrj[nn + 1] - nrj[mm]) && (x[nn - 1] - x[mm] >= nrj[nn] - nrj[mm]) && (nn <= n))
             n0 = i + 1;
-        else if ((x[nn] - x[mmm] <= nrj[nn + 1] - nrj[mmm]) && (x[nn - 1] - x[mmm] <= nrj[nn] - nrj[mmm]) && (nn <= n))
+        else if ((x[nn] - x[mm] <= nrj[nn + 1] - nrj[mm]) && (x[nn - 1] - x[mm] <= nrj[nn] - nrj[mm]) && (nn <= n))
             nn = i; 
         else if (nn >= n);
-            return zoloto[nn] - zoloto[mmm];
+            return zoloto[nn] - zoloto[mm];
     }
     return -1;
 }
