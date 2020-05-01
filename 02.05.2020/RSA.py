4cc0960c41d67588bfd963ad07eefd9f36de1529
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2020-05-01 21:26:43 +0300

    Update RSA.py

--- 02.05.2020/RSA.py
+++ 02.05.2020/RSA.py
@@ -61,7 +61,7 @@ def encryption(m, e, n):
     return c
 
 def decryption(c, d, n):
-    m = ((c % n) ** d7) % n
+    m = pow(c, d, n)
     return m
 
 print('Введите шифруемое сообщение (число)')
@@ -73,5 +73,5 @@ print('\nКлючи: e = ' + str(a[0]) + ' n = ' + str(a[1]) + ' d = ' + str (a[
 c = encryption(m, a[0], a[1])
 print('Зашифрованное сообщение: ' + str(c))
 
-#m = decryption(c, a[2], a[1]) #долго
-#print('Расшифрованное сообщение: ' + str(m))
+m = decryption(c, a[2], a[1]) #долго
+print('Расшифрованное сообщение: ' + str(m))
